cbbbf31536c300fa4c4ea44267f3c9e1beffc434
Tyquan <tyquanjreddick@gmail.com>
2022-02-07 16:11:58 -0500

    update index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Weemaple' });
+  res.render('index', { title: 'Weemaple API' });
 });
 
 module.exports = router;
