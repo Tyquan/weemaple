a6dad8061b80f896fda03e7e3cd0692f59fa9973
Tyquan Reddick <Tyquanjreddick@gmail.com>
2022-10-11 13:08:11 -0400

    fixed favicon error

--- app.js
+++ app.js
@@ -19,12 +19,14 @@ const messages = require('./API/contactMessageApi');
 const app = express();
 
 Middleware.init(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public/images/fav', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));  
 
 // view engine setupnpm 
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+
 
 
 app.use('/', index);
