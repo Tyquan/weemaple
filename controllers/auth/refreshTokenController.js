af7da36bce0a334aa129593f28df2c1abfc23d21
Tyquan Reddick <Tyquanjreddick@gmail.com>
2022-10-12 09:48:28 -0400

    changed models -> Models (deploy)

--- controllers/auth/refreshTokenController.js
+++ controllers/auth/refreshTokenController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const User = require('../../models/User');
+const User = require('../../Models/User');
 
 const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
