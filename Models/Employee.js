a7fe658448091bfca0955df4082292faee2774b2
Tyquan Reddick <Tyquanjreddick@gmail.com>
2023-03-04 12:49:36 -0500

    added employee model

---
+++ Models/Employee.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EmployeeSchema = Schema({
+    userId: {
+        type: String
+    },
+    organizationId: {
+        type: String
+    },
+    roleId: {type: String},
+    createdBy: {type: String},
+    updatedBy: {type: String},
+    code: {type: String},
+    status: {type: String},
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startsAt: {
+        type: Date
+    },
+    endsAt: {
+        type: Date
+    },
+    notes: {type: String}
+}, {strict: false});
+
+module.exports = mongoose.model("Employee", EmployeeSchema);
