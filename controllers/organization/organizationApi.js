8d82c62f4a9069d318107d1e6493120ca0cba02d
Tyquan Reddick <Tyquanjreddick@gmail.com>
2023-03-04 14:18:10 -0500

    updated orginization controller

--- controllers/organization/organizationApi.js
+++ controllers/organization/organizationApi.js
@@ -4,5 +4,11 @@ const organizationController = require('./organizationController');
 
 organizationApi.route('/')
     .get(organizationController.getOrganizations)
+    .post(organizationController.createNewOrganization)
+    .put(organizationController.updateOrganization)
+    .delete(organizationController.deleteOrganization);
+organizationApi.route('/:id')
+    .get(organizationController.getSingleOrganization);
+
 
 module.exports = organizationApi;
--- controllers/organization/organizationController.js
+++ controllers/organization/organizationController.js
@@ -8,6 +8,36 @@ const getOrganizations = (req, res) => {
     })
 };
 
+const getSingleOrganization = (req, res) => {
+    databaseApi.GetSingleData(organizationModel, req.params.id, (organization, error) => {
+        if (error) res.status(500).json(error);
+        res.status(200).json(organization);
+    });
+};
+
+const createNewOrganization = (req, res) => {
+    databaseApi.PostData(organizationModel, req, (data, error) => {
+        if (error) res.status(500).json(error);
+        res.json(data);
+    });
+};
+
+const updateOrganization = (req, res) => {
+    databaseApi.UpdateData(organizationModel, req, req.body, (data) => {
+        res.json(data);
+    })
+};
+
+const deleteOrganization = (req, res) => {
+    databaseApi.DeleteData(organizationModel, req, (data) => {
+        res.json(data);
+    })
+};
+
 module.exports = {
-    getOrganizations
+    getOrganizations,
+    createNewOrganization,
+    getSingleOrganization,
+    updateOrganization,
+    deleteOrganization
 };
